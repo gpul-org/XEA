66cdc1a5b32f858dc5af623282332d14ccac96b6
Ojocuidao <a.todaro.lcg@gmail.com>
2016-08-12 23:39:51 +0200

    Core of the registration feature and test

---
+++ xea-core/api/serializers.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ('id',
+                  'username',
+                  'password',
+                  'email',
+                  'first_name',
+                  'last_name')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        user = get_user_model().objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            password = validated_data.pop('password')
+            instance.set_password(password)
+        return super(UserSerializer, self).update(instance, validated_data)
+
--- xea-core/api/views.py
+++ xea-core/api/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions
+from rest_framework import status
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from . import serializers
+
+
+class RegisterUserView(CreateAPIView):
+
+    model = get_user_model() # We actually use the standard User model
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = serializers.UserSerializer
+'''
+    def get(self, request):
+        return Response({"msg": "Use a POST request to access this resource."})
+
+    def post(self, request):
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+'''
